3eede238e9016654c778b4aa2ab0664591a6948d
KwalaCode <saberyassineback@gmail.com>
2024-11-19 22:25:30 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,42 +1,28 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from os import path
 from flask_login import LoginManager
+import logging
 
 db = SQLAlchemy()
-DB_NAME = "database.db"
-
-
+login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'kwalaistheshit'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SECRET_KEY'] = 'your-secret-key'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///your_database.db'  # Update this with your database URI
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
     db.init_app(app)
-
-    from .views import views
-    from .auth import auth
-
-    app.register_blueprint(views, url_prefix='/')
-    app.register_blueprint(auth, url_prefix='/')
-
-    from .models import User, Note
-    
-    with app.app_context():
-        db.create_all()
-
-    login_manager = LoginManager()
-    login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
 
-    @login_manager.user_loader
-    def load_user(id):
-        return User.query.get(int(id))
+    # Set up logging
+    logging.basicConfig(level=logging.INFO)
+
+    # Import and register blueprints
+    from .routes import main, auth, booking, admin
+    app.register_blueprint(main)
+    app.register_blueprint(auth)
+    app.register_blueprint(booking)
+    app.register_blueprint(admin)
 
     return app
-
-
-def create_database(app):
-    if not path.exists('website/' + DB_NAME):
-        db.create_all(app=app)
-        print('Created Database!')
